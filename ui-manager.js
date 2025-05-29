b33ce82752db1a3d7789a4a163f127828628aa5e
MELIORO-systems <pavel@melioro.cz>
2025-05-29 23:17:10 +0200

    Update ui-manager.js

--- ui-manager.js
+++ ui-manager.js
@@ -878,28 +878,3 @@ export class UIManager extends EventEmitter {
         }
     }
 }
-
-    on(event, listener) {
-        if (!this.events[event]) {
-            this.events[event] = [];
-        }
-        this.events[event].push(listener);
-    }
-
-    off(event, listener) {
-        if (!this.events[event]) return;
-        
-        const index = this.events[event].indexOf(listener);
-        if (index > -1) {
-            this.events[event].splice(index, 1);
-        }
-    }
-
-    emit(event, ...args) {
-        if (!this.events[event]) return;
-        
-        this.events[event].forEach(listener => {
-            listener(...args);
-        });
-    }
-}
